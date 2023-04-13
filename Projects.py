313963b09546a428f009e9c2580bdb8ce0cc68ba
Louis <louis.dupand@gmail.com>
2023-04-13 14:06:52 +0200

    New way to stock the projects

---
+++ Projects.py
@@ -0,0 +1,8 @@
+PROJECTS = {
+    "🏆 Scriptaly creates Python code to import datasets":"https://scriptaly.streamlit.app/",
+    "🏆 Defi Strategy":"https://kryptosphere-defi-strategy.streamlit.app/",
+    "🏆 Kickstarter Dashboard": "https://leo-dujourd-hui-kickstarter-dash.streamlit.app/",
+    "🏆 Prediction of the number of rented bike in seoul": "https://github.com/le-cmyk/Seoul_Bike",
+    "🏆 Visualisattion of the railway system in France": "https://github.com/le-cmyk/SNCF",
+    "🏆 Digital resume": "https://github.com/le-cmyk/Digital-CV",
+}
--- app.py
+++ app.py
@@ -32,14 +32,8 @@ SOCIAL_MEDIA = {
     "LinkedIn": "https://www.linkedin.com/in/leo-dujourd-hui/",
     "GitHub": "https://github.com/le-cmyk",
 }
-PROJECTS = {
-    "🏆 Scriptaly creates Python code to import datasets":"https://scriptaly.streamlit.app/",
-    "🏆 Defi Strategy":"https://kryptosphere-defi-strategy.streamlit.app/",
-    "🏆 Kickstarter Dashboard": "https://leo-dujourd-hui-kickstarter-dash.streamlit.app/",
-    "🏆 Prediction of the number of rented bike in seoul": "https://github.com/le-cmyk/Seoul_Bike",
-    "🏆 Visualisattion of the railway system in France": "https://github.com/le-cmyk/SNCF",
-    "🏆 Digital resume": "https://github.com/le-cmyk/Digital-CV",
-}
+
+from Projects import PROJECTS
 
 
 
